6e5b262cd115067a8773fc993b3c0a7b99fee96b
Philipp Hofmann <philipp.hofmann@iws.uni-stuttgart.de>
2026-03-27 11:37:01 +0100

    Refactor boundary check using is_in_interval method

--- exercise3.py
+++ exercise3.py
@@ -32,8 +32,7 @@ class Rectangle:
         #         and reuse that here.
         ll_px = point.x - self._lower_left.x
         ll_py = point.y - self._lower_left.y
-        return ll_px >= 0 and ll_px <= self._dx \
-            and ll_py >= 0 and ll_py <= self._dy
+        return self.is_in_interval(ll_px, 0.0, self._dx) and self.is_in_interval(ll_py, 0.0, self._dy)
 
     # def _is_idx_on_upper_edge(self, i: int) -> bool:
     #     return i in [2, 3]
